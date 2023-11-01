6bdde01e52355fdb617961e1dc3b76dbfb7e8aaf
julia <julia.mejia@mail.escuelaing.edu.co>
2023-11-01 17:34:23 -0500

    MVP SketchSync

---
+++ target/classes/static/JS/controlUsers.js
@@ -0,0 +1,14 @@
+async function signIn() {
+    const name = prompt("Ingrese su nombre de usuario:"); // Pide al usuario su nombre
+    if (name) { // Comprueba si el usuario proporcionó un nombre
+        localStorage.setItem("logeo", name);
+
+        if (name === "Organizer") {
+            app.createOrganizer(name);
+        } else {
+            app.createUser(name);
+        }
+    } else {
+        console.log("El usuario no proporcionó un nombre.");
+    }
+}
